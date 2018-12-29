fab7cae27aaff24741f54681306a38964aa5cd42
veteranslayer <37368982+veteranslayer@users.noreply.github.com>
2018-12-30 01:29:46 +0200

    Added print componet
    Added user Control PrintQuestion that will be used at TempPrint for printing a quiz

---
+++ QuizMaker/QuizMaker/PrintQuestion.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuizMaker
+{
+    public partial class PrintQuestion : UserControl
+    {
+        public PrintQuestion()
+        {
+            InitializeComponent();
+        }
+
+        public void setQuestionText(string text, int answerNum)
+        {
+            switch (answerNum)
+            {
+                case 1:
+                    answer1.Text = text;
+                    break;
+                case 2:
+                    answer2.Text = text;
+                    break;
+                case 3:
+                    answer3.Text = text;
+                    break;
+                case 4:
+                    answer4.Text = text;
+                    break;
+            }
+            foreach(Label l in this.Controls)
+            {
+                if(l.Text == "undefined")
+                {
+                    l.Visible = false;
+                }
+            }
+        }
+
+        public void setTitle(string title)
+        {
+            QuestionNameLabel.Text = title;
+        }
+    }
+}
--- QuizMaker/QuizMaker/TempPrintControl.Designer.cs
+++ QuizMaker/QuizMaker/TempPrintControl.Designer.cs
@@ -29,9 +29,10 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.titleLabel = new System.Windows.Forms.Label();
-            this.namePosLabel = new System.Windows.Forms.Label();
             this.surnamePosLabel = new System.Windows.Forms.Label();
+            this.namePosLabel = new System.Windows.Forms.Label();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -45,15 +46,15 @@
             this.panel1.Size = new System.Drawing.Size(654, 157);
             this.panel1.TabIndex = 0;
             // 
-            // titleLabel
+            // surnamePosLabel
             // 
-            this.titleLabel.AutoSize = true;
-            this.titleLabel.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
-            this.titleLabel.Location = new System.Drawing.Point(15, 11);
-            this.titleLabel.Name = "titleLabel";
-            this.titleLabel.Size = new System.Drawing.Size(69, 24);
-            this.titleLabel.TabIndex = 0;
-            this.titleLabel.Text = "label1";
+            this.surnamePosLabel.AutoSize = true;
+            this.surnamePosLabel.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.surnamePosLabel.Location = new System.Drawing.Point(192, 99);
+            this.surnamePosLabel.Name = "surnamePosLabel";
+            this.surnamePosLabel.Size = new System.Drawing.Size(86, 22);
+            this.surnamePosLabel.TabIndex = 2;
+            this.surnamePosLabel.Text = "Surname";
             // 
             // namePosLabel
             // 
@@ -65,21 +66,29 @@
             this.namePosLabel.TabIndex = 1;
             this.namePosLabel.Text = "Name";
             // 
-            // surnamePosLabel
+            // titleLabel
             // 
-            this.surnamePosLabel.AutoSize = true;
-            this.surnamePosLabel.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
-            this.surnamePosLabel.Location = new System.Drawing.Point(192, 99);
-            this.surnamePosLabel.Name = "surnamePosLabel";
-            this.surnamePosLabel.Size = new System.Drawing.Size(86, 22);
-            this.surnamePosLabel.TabIndex = 2;
-            this.surnamePosLabel.Text = "Surname";
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.titleLabel.Location = new System.Drawing.Point(15, 11);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(69, 24);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "label1";
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(0, 163);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(650, 540);
+            this.panel2.TabIndex = 3;
             // 
             // TempPrintControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Name = "TempPrintControl";
             this.Size = new System.Drawing.Size(654, 705);
@@ -95,5 +104,6 @@
         private System.Windows.Forms.Label surnamePosLabel;
         private System.Windows.Forms.Label namePosLabel;
         private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Panel panel2;
     }
 }
