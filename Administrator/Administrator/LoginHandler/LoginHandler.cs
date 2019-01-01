7d1db497784e5d1b9fd0c9ca76da0c0e0e151d9a
panosNeo <panosn30@gmail.com>
2019-01-01 02:50:05 +0200

    created login handler

---
+++ Administrator/Administrator/LoginHandler/LoginHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Administrator.LoginHandler
+{
+    class LoginHandler
+    {
+        private string username;
+        private string password;
+        private bool stayLog;
+
+        public LoginHandler(string username,string password,bool stayLog)
+        {
+            this.username = username;
+            this.password = password;
+            this.stayLog = stayLog;
+        }
+
+        public void SearchAccount()
+        {
+
+        }
+
+        private void StayIn() {
+            //checkarw gia na ton afhsw in
+        }
+    }
+}
---
+++ Administrator/Administrator/LoginHandler/Profile.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Administrator.LoginHandler
+{
+    class Profile
+    {
+        private string username;
+        private string password;
+        private string name;
+        private string lastname;
+        private string email;
+        private string role;
+
+        public Profile(string username,string password,string name,string lastname, string email, string role)
+        {
+            this.username = username;
+            this.password = password;
+            this.name = name;
+            this.lastname = lastname;
+            this.email = email;
+            this.role = role;
+        }
+
+        public string getUsername()
+        {
+            return username;
+        }
+
+        public string getPassword()
+        {
+            return password;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public string getLastname()
+        {
+            return lastname;
+        }
+
+        public string getEmail()
+        {
+            return email;
+        }
+
+        public string getRole()
+        {
+            return role;
+        }
+    }
+}
