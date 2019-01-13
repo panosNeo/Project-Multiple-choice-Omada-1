bbc33aa7d3357a15d7137e3b03bc01ca64233dbb
George Trachanas <44811733+Jorgy27@users.noreply.github.com>
2019-01-13 21:57:57 +0200

    Update

--- QuizMaker/QuizMaker/MultipleChoiceDataSet.Designer.cs
+++ QuizMaker/QuizMaker/MultipleChoiceDataSet.Designer.cs
@@ -9408,14 +9408,13 @@ namespace QuizMaker.MultipleChoiceDataSetTableAdapters {
             this._commandCollection[0].CommandType = global::System.Data.CommandType.Text;
             this._commandCollection[1] = new global::System.Data.OleDb.OleDbCommand();
             this._commandCollection[1].Connection = this.Connection;
-            this._commandCollection[1].CommandText = "INSERT INTO `Report` (`User_id`, `Report`, `Question_id`, `Quiz_id`, `Delete`) VA" +
-                "LUES (?, ?, ?, ?, ?)";
+            this._commandCollection[1].CommandText = "INSERT INTO Report\r\n                         (User_id, Report, Question_id, Quiz_" +
+                "id, Report_id)\r\nVALUES        (?, ?, NULL, ?, ?)";
             this._commandCollection[1].CommandType = global::System.Data.CommandType.Text;
             this._commandCollection[1].Parameters.Add(new global::System.Data.OleDb.OleDbParameter("User_id", global::System.Data.OleDb.OleDbType.Integer, 0, global::System.Data.ParameterDirection.Input, ((byte)(0)), ((byte)(0)), "User_id", global::System.Data.DataRowVersion.Current, false, null));
             this._commandCollection[1].Parameters.Add(new global::System.Data.OleDb.OleDbParameter("Report", global::System.Data.OleDb.OleDbType.WChar, 255, global::System.Data.ParameterDirection.Input, ((byte)(0)), ((byte)(0)), "Report", global::System.Data.DataRowVersion.Current, false, null));
-            this._commandCollection[1].Parameters.Add(new global::System.Data.OleDb.OleDbParameter("Question_id", global::System.Data.OleDb.OleDbType.Integer, 0, global::System.Data.ParameterDirection.Input, ((byte)(0)), ((byte)(0)), "Question_id", global::System.Data.DataRowVersion.Current, false, null));
             this._commandCollection[1].Parameters.Add(new global::System.Data.OleDb.OleDbParameter("Quiz_id", global::System.Data.OleDb.OleDbType.Integer, 0, global::System.Data.ParameterDirection.Input, ((byte)(0)), ((byte)(0)), "Quiz_id", global::System.Data.DataRowVersion.Current, false, null));
-            this._commandCollection[1].Parameters.Add(new global::System.Data.OleDb.OleDbParameter("Delete", global::System.Data.OleDb.OleDbType.Variant, 1024, global::System.Data.ParameterDirection.Input, ((byte)(0)), ((byte)(0)), "Delete", global::System.Data.DataRowVersion.Current, false, null));
+            this._commandCollection[1].Parameters.Add(new global::System.Data.OleDb.OleDbParameter("Report_id", global::System.Data.OleDb.OleDbType.Integer, 0, global::System.Data.ParameterDirection.Input, ((byte)(0)), ((byte)(0)), "Report_id", global::System.Data.DataRowVersion.Current, false, null));
         }
         
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
@@ -9636,7 +9635,7 @@ namespace QuizMaker.MultipleChoiceDataSetTableAdapters {
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "15.0.0.0")]
         [global::System.ComponentModel.Design.HelpKeywordAttribute("vs.data.TableAdapter")]
         [global::System.ComponentModel.DataObjectMethodAttribute(global::System.ComponentModel.DataObjectMethodType.Insert, false)]
-        public virtual int InsertReport(int User_id, string Report, global::System.Nullable<int> Question_id, global::System.Nullable<int> Quiz_id, object Delete) {
+        public virtual int InsertReport(int User_id, string Report, global::System.Nullable<int> Quiz_id, int Report_id) {
             global::System.Data.OleDb.OleDbCommand command = this.CommandCollection[1];
             command.Parameters[0].Value = ((int)(User_id));
             if ((Report == null)) {
@@ -9645,24 +9644,13 @@ namespace QuizMaker.MultipleChoiceDataSetTableAdapters {
             else {
                 command.Parameters[1].Value = ((string)(Report));
             }
-            if ((Question_id.HasValue == true)) {
-                command.Parameters[2].Value = ((int)(Question_id.Value));
+            if ((Quiz_id.HasValue == true)) {
+                command.Parameters[2].Value = ((int)(Quiz_id.Value));
             }
             else {
                 command.Parameters[2].Value = global::System.DBNull.Value;
             }
-            if ((Quiz_id.HasValue == true)) {
-                command.Parameters[3].Value = ((int)(Quiz_id.Value));
-            }
-            else {
-                command.Parameters[3].Value = global::System.DBNull.Value;
-            }
-            if ((Delete == null)) {
-                throw new global::System.ArgumentNullException("Delete");
-            }
-            else {
-                command.Parameters[4].Value = ((object)(Delete));
-            }
+            command.Parameters[3].Value = ((int)(Report_id));
             global::System.Data.ConnectionState previousConnectionState = command.Connection.State;
             if (((command.Connection.State & global::System.Data.ConnectionState.Open) 
                         != global::System.Data.ConnectionState.Open)) {
